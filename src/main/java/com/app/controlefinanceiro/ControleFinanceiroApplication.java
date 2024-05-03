cfa93e6396fb67990937d88e0a34101ccfe5c168
Fernando97Rocha <contato.dev.fernando@gmail.com>
2024-05-03 17:21:35 -0300

    Modeled entities for the application

--- src/main/java/com/app/controlefinanceiro/ControleFinanceiroApplication.java
+++ src/main/java/com/app/controlefinanceiro/ControleFinanceiroApplication.java
@@ -3,11 +3,15 @@ package com.app.controlefinanceiro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ControleFinanceiroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleFinanceiroApplication.class, args);
-	}
 
+		System.out.println("Hello World");
+
+	}
 }
---
+++ src/main/java/com/app/controlefinanceiro/model/income/Income.java
@@ -0,0 +1,75 @@
+package com.app.controlefinanceiro.model.income;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Income {
+
+    private Long id;
+    private String description;
+    private BigDecimal value;
+    private LocalDateTime creationDate;
+    private int category;
+    private Long userId;
+
+    public Income() {
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public Income(Long id, String description, BigDecimal value, LocalDateTime creationDate, int category, Long userId) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.creationDate = LocalDateTime.now();
+        this.category = category;
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
