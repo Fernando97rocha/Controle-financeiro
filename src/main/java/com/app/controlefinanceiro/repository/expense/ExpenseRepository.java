7e5ca7dc14429ce6edaeaaef03f23d573ea1255c
Fernando97Rocha <contato.dev.fernando@gmail.com>
2024-05-04 09:13:05 -0300

    created repositories of entities

---
+++ src/main/java/com/app/controlefinanceiro/repository/expense/ExpenseRepository.java
@@ -0,0 +1,10 @@
+package com.app.controlefinanceiro.repository.expense;
+
+import com.app.controlefinanceiro.model.expense.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    Optional<Expense> findByDescription();
+}
