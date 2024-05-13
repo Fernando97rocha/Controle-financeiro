ef071a5dec54d926e708cc8bf325455ef777eb75
Fernando97Rocha <contato.dev.fernando@gmail.com>
2024-05-13 16:02:48 -0300

    created user autorization service (no implements)

--- src/main/java/com/app/controlefinanceiro/repository/user/UserRepository.java
+++ src/main/java/com/app/controlefinanceiro/repository/user/UserRepository.java
@@ -1,10 +1,9 @@
-//package com.app.controlefinanceiro.repository.user;
-//
-//import com.app.controlefinanceiro.model.user.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    Optional<User> findByEmail();
-//}
+package com.app.controlefinanceiro.repository.user;
+
+import com.app.controlefinanceiro.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByEmail();
+}
