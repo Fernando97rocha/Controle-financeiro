1288b6dda94813fdbe97dd24ae54c94fcd862b71
Fernando97Rocha <contato.dev.fernando@gmail.com>
2024-05-19 23:55:38 -0300

    JWT maven dependency added

--- src/main/java/com/app/controlefinanceiro/infra/security/SecurityConfiguration.java
+++ src/main/java/com/app/controlefinanceiro/infra/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.app.controlefinanceiro.infra;
+package com.app.controlefinanceiro.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
